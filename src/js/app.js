495617a38d7e1c3829dad79b38f7e6c5ce512043
Benjamin Baum <bjb253@cornell.edu>
2017-12-11 23:05:00 -0500

    Pass in addl params

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 'use strict'
 
-console.log('PARATII EXPERIMENTS')
 
 const Clappr = require('clappr')
 const Controls = require('./controls')
 
-module.exports = ({ source, selector, mimeType }) => new Clappr.Player({
+module.exports = ({ source, selector, mimeType, ...rest }) => new Clappr.Player({
   source,
   mimeType,
   parentId: selector,
   mediacontrol: { external: Controls },
+  ...rest
 });
