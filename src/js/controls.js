9b7217b2d86d063ee7b7d9f97bffc94cac686cdf
Benjamin Baum <bjb253@cornell.edu>
2017-12-11 19:58:34 -0500

    Remove UI overrides

--- src/js/controls.js
+++ src/js/controls.js
@@ -3,50 +3,50 @@ const MediaControl = require('../html/NewMediaControl.html')
 const styles = require('styles/controls.scss')
 
 class ParatiiMediaControl extends Clappr.MediaControl {
-  get template() {
-    return Clappr.template(
-      MediaControl
-    )
-  }
+  // get template() {
+  //   return Clappr.template(
+  //     MediaControl
+  //   )
+  // }
 
-  get stylesheet () {
-    return Clappr.Styler.getStyleFor(
-      ``
-    )
-  }
+  // get stylesheet () {
+  //   return Clappr.Styler.getStyleFor(
+  //     ``
+  //   )
+  // }
 
   constructor(options = {}) {
     super(options);
   }
 
-  addEventListeners() {
-    super.addEventListeners();
-    if (this.container) {
-      this.listenTo(this.container, Clappr.Events.CONTAINER_PLAY, this.updatePlayPauseButton)
-      this.listenTo(this.container, Clappr.Events.CONTAINER_PAUSE, this.updatePlayPauseButton)
-    }
-  }
+  // addEventListeners() {
+  //   super.addEventListeners();
+  //   if (this.container) {
+  //     this.listenTo(this.container, Clappr.Events.CONTAINER_PLAY, this.updatePlayPauseButton)
+  //     this.listenTo(this.container, Clappr.Events.CONTAINER_PAUSE, this.updatePlayPauseButton)
+  //   }
+  // }
 
-  initializeIcons() {
-    super.initializeIcons();
-    this.updatePlayPauseButton();
-  }
+  // initializeIcons() {
+  //   super.initializeIcons();
+  //   this.updatePlayPauseButton();
+  // }
 
-  updatePlayPauseButton() {
-    if (!this.container) {
-      return;
-    }
-    if (this.container.isPlaying()) {
-      this.$playPauseToggle.removeClass('video-paused');
-    } else {
-      this.$playPauseToggle.addClass('video-paused');
-    }
-  }
+  // updatePlayPauseButton() {
+  //   if (!this.container) {
+  //     return;
+  //   }
+  //   if (this.container.isPlaying()) {
+  //     this.$playPauseToggle.removeClass('video-paused');
+  //   } else {
+  //     this.$playPauseToggle.addClass('video-paused');
+  //   }
+  // }
 
-  togglePlayPause() {
-    super.togglePlayPause();
-    this.updatePlayPauseButton();
-  }
+  // togglePlayPause() {
+  //   super.togglePlayPause();
+  //   this.updatePlayPauseButton();
+  // }
 }
 
 module.exports = ParatiiMediaControl;
