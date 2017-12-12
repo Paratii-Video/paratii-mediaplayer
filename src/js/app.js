54d3891751d0912488a86993a8121c9895e54c44
Benjamin Baum <bjb253@cornell.edu>
2017-12-11 20:53:14 -0500

    Fix export

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,9 @@ console.log('PARATII EXPERIMENTS')
 const Clappr = require('clappr')
 const Controls = require('./controls')
 
-const vid = new Clappr.Player({
-  // source: 'https://gateway.paratii.video/ipfs/QmNZS5J3LS1tMEVEP3tz3jyd2LXUEjkYJHyWSuwUvHDaRJ', // original mp4
-  source: '/public/video/tsn.mp4', // transcoded multi-bitrate HLS
-  // source: '/master.m3u8', // transcoded multi-bitrate HLS
-  parentId: '#player',
-  mimeType: 'video/mp4',
-  watermark: 'http://paratii.video/imagens/cropped-logo_colorido_horizontal.png',
-  watermarkPosition: 'top-right',
-  watermarkLink: 'http://paratii.video/',
-  hideMediaControl: false,
+module.exports = ({ source, selector, mimeType }) => new Clappr.Player({
+  source,
+  mimeType,
+  parentId: selector,
   mediacontrol: { external: Controls },
-})
+});
