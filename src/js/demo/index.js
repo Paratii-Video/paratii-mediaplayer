a952946c7bacc70e2e77aa507e7a147b5b63f10d
Benjamin Baum <bjb253@cornell.edu>
2018-01-08 17:56:24 -0500

    13 - add static demo video for faster dev experience/load time

--- src/js/demo/index.js
+++ src/js/demo/index.js
@@ -4,9 +4,6 @@ require("../../styles/demo/app.scss");
 
 CreatePlayer({
   selector: "#player",
-  source:
-    "https://gateway.paratii.video" +
-    "/ipfs/Qmcw1YXdtmik4KsELVjVW25T5F9EZRVYefATXhbWDYK19t" +
-    "/master.m3u8",
+  source: "assets/video/ParatiiDemo.mp4",
   ipfsHash: "Qmcw1YXdtmik4KsELVjVW25T5F9EZRVYefATXhbWDYK19t"
 });
